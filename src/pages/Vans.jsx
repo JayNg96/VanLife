05bf313052ed1e87f4c7ed4812fc99e7be1a0188
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-08-07 22:57:48 +0800

    Add vans detail page with Link and useParams

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,4 @@
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import React from 'react'
 
 export function Vans() {
@@ -11,12 +11,14 @@ export function Vans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} alt="van"/>
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} alt="van"/>
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
