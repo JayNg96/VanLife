078cd2069f38fe543f5970fc81d5ec175ef77178
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-08-09 01:16:03 +0800

    Display vans detail

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,36 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
 export function HostVanDetail() {
+
+    const { id } = useParams()
+    const [currentVan, setHostVan] = React.useState(null)
+
+    React.useEffect(()=>{
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setHostVan(data.vans))
+            // eslint-disable-next-line
+    }, [])
+    
+    if(currentVan){
+        console.log(currentVan.id)
+    }
+    
     return (
-        <h1>Back to all vans</h1>
+        <>
+            {
+                currentVan ?
+                <div>
+                    <img src={currentVan.imageUrl} width={150} alt="current van"/>
+                    <h2>{currentVan.name}</h2>
+                    <p>{currentVan.price}</p>
+                    <p>{currentVan.type}</p>
+                </div>
+                :
+                <h1>Loading...</h1>
+            }
+        </>
     )
+
 };
