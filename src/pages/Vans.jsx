40669b2575a77d5aa58edb89410d50a453526560
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-08-12 15:13:11 +0800

    Add search filters using useSearchParams

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import React from 'react'
 
 export function Vans() {
+
+
     const [vans, setVans] = React.useState([])
     React.useEffect(() => {
         fetch("/api/vans")
@@ -9,7 +11,12 @@ export function Vans() {
             .then(data => setVans(data.vans))
     }, [])
 
-    const vanElements = vans.map(van => (
+    // eslint-disable-next-line
+    const [searchParams, setSearchParams] = useSearchParams()
+    const typeFilter = searchParams.get("type")
+    const displayedVans = typeFilter ? vans.filter(van => van.type === typeFilter) : vans
+
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} alt="van"/>
@@ -25,6 +32,24 @@ export function Vans() {
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <button 
+                    onClick={() => setSearchParams({type: "simple"})}
+                    className="van-type simple"
+                >Simple</button>
+                <button 
+                    onClick={() => setSearchParams({type: "luxury"})}
+                    className="van-type luxury"
+                >Luxury</button>
+                <button 
+                    onClick={() => setSearchParams({type: "rugged"})}
+                    className="van-type rugged"
+                >Rugged</button>
+                <button 
+                    onClick={() => setSearchParams({})}
+                    className="van-type clear-filters"
+                >Clear filter</button>
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
