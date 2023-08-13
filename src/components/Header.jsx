a6edeee93d569747f46e487ea3e79d00156dae1d
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-08-13 20:35:34 +0800

    Add login page & styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { NavLink, Link } from 'react-router-dom'
+import avatarIcon from '../assets/images/avatar-icon.png'
 
 export function Header() {
     return (
@@ -9,6 +10,9 @@ export function Header() {
                 <NavLink className={({isActive}) => isActive ? "nav-selected" : ""} to="/host">Host</NavLink>
                 <NavLink className={({isActive}) => isActive ? "nav-selected" : ""} to="/about">About</NavLink>
                 <NavLink className={({isActive}) => isActive ? "nav-selected" : ""} to="/vans">Vans</NavLink>
+                <Link to="login" className="login-link">
+                    <img src={avatarIcon} className="login-icon" alt="login"/>
+                </Link>
             </nav>
         </header>
     )
